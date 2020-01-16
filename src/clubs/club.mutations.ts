83babf1adecb0a2e40cc075d29ff3640980e38ec
jun <junho_hong@brown.edu>
2020-01-15 22:30:50 -0500

    comment out error

--- src/clubs/club.mutations.ts
+++ src/clubs/club.mutations.ts
@@ -5,30 +5,30 @@ import { MutationResolvers } from "resolvers-types"
 export const _mutations: MutationResolvers = {
     club: (root, args, ctx) => {
         const creator: MUser = ctx.user
-        if (creator == undefined) {
-            return Error("You don't have permission to create event.")
-        }
+        // if (creator == undefined) {
+        //     return Error("You don't have permission to create event.")
+        // }
 
         const input: CreateClubInput = args.input
         const club = MClubBuilder.createFromInput(input)
-        if (club.title === "") {
-            return Error("Title is empty")
-        }
-        if (club.description === "") {
-            return Error("Description is empty")
-        }
-        if (club.time.startAt === 0) {
-            return Error("When does the event  start?")
-        }
-        if (club.time.duration === 0) {
-            return Error("How long does the event  last?")
-        }
-        if (club.location.address === "") {
-            return Error("Where does the event happen?")
-        }
-        if (club.location.lat === 0 || club.location.long === 0) {
-            return Error("Invalid location address")
-        }
+        // if (club.title === "") {
+        //     return Error("Title is empty")
+        // }
+        // if (club.description === "") {
+        //     return Error("Description is empty")
+        // }
+        // if (club.time.startAt === 0) {
+        //     return Error("When does the event  start?")
+        // }
+        // if (club.time.duration === 0) {
+        //     return Error("How long does the event  last?")
+        // }
+        // if (club.location.address === "") {
+        //     return Error("Where does the event happen?")
+        // }
+        // if (club.location.lat === 0 || club.location.long === 0) {
+        //     return Error("Invalid location address")
+        // }
         if (club.slotCount === undefined) {
             club.slotCount = 0
         }
@@ -41,6 +41,6 @@ export const _mutations: MutationResolvers = {
         club.host = host
         let sql = new ClubSQL()
         const newClub = sql.create(club)
-        return newClub
+        return club
     }
 }
