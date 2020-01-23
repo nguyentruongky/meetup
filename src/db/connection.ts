10a3a6c5ecc1051da9f38436694ef23e815b0266
Ky Nguyen <kynguyen@Kys-MacBook-Pro.local>
2020-01-24 00:43:51 +0700

    Ignore database configuration

---
+++ src/db/connection.ts
@@ -0,0 +1,12 @@
+const { Client } = require("pg")
+import { config } from "./configuration"
+
+export const runQuery = async (queryString: string) => {
+    console.log("running query: \n" + queryString)
+
+    const client = new Client(config)
+    client.connect()
+    const res = await client.query(queryString)
+    return res
+} 
+
